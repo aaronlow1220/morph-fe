9d7df0f31b8b982af248bea2eb4bd154da664abc
aaronlow1220 <117196390+aaronlow1220@users.noreply.github.com>
2025-02-06 17:34:46 +0800

    feat: platform functionality

---
+++ public/js/platform.js
@@ -0,0 +1,38 @@
+function createPlatform() {
+	const url = `${BASE_URL}/platform`;
+	const label = document.getElementById("label");
+	const name = document.getElementById("name");
+	const sftp = document.getElementById("sftp");
+	const priceCurrency = document.getElementById("price_currency");
+	const inputContainer = document.getElementById("to_form");
+	const inputs = inputContainer.querySelectorAll("input");
+	const data = {};
+
+	inputs.forEach((input) => {
+		data[input.id] = input.value;
+	});
+
+	const jsonStructure = {
+		name: name.value,
+		label: label.value,
+		data: JSON.stringify(data),
+		sftp: String(Number(sftp.checked)),
+		price_currency: String(Number(priceCurrency.checked)),
+	};
+
+	fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${ACCESS_TOKEN}`,
+		},
+		body: JSON.stringify(jsonStructure),
+	})
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error();
+			}
+			stateModal("success", "成功新增一筆資料");
+		})
+		.catch((error) => stateModal("failed", "失敗，請再試一次"));
+}
